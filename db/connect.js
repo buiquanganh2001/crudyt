c32fdf9a98531c1412991d9f2e986b1876f5be20
Quang Anh Bùi <buiquanganh2001@gmail.com>
2021-12-30 15:49:47 +0700

    This is a new commit for what I originally planned to be amended

---
+++ db/connect.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDb = (url) => {
+    return mongoose.connect (url, { 
+        useNewUrlParser: true,
+        
+        useUnifiedTopology: true
+    })
+}
+
+
+
+module.exports =  connectDb;
